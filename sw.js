9be7fc42092aa646f475aca7921e7f7cb5b07626
董文博 <wenbo.dong@shangri-la.com>
2020-08-06 16:10:37 +0800

    feat: update design blog

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e311edf5e3cf33baac54.js"
+    "url": "webpack-runtime-db6d4548dab876c92380.js"
   },
   {
     "url": "app.76e6c55a11ab238cc939.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6740cd7043946f539bf0a91f6da12a06"
+    "revision": "21d5aa6ff20c0c01a3e5e6746038ac15"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
